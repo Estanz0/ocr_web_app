d1f8678f340c9a93b0f383a7e0c7341380359ffd
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2023-12-26 13:30:50 +1300

    basic frontend

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ImageOCR from './components/ImageOCR';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageOCR />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/app/src/components/ImageOCR.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+
+function ImageOCR() {
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
+    const [uploadedImageUrl, setUploadedImageUrl] = useState<string | null>(null);
+    const [parsedText, setParsedText] = useState<string | null>(null);
+
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (selectedImage) {
+            const formData = new FormData();
+            formData.append('image', selectedImage);
+            formData.append('filename', selectedImage.name);
+
+            // Send the POST request with the image and filename
+            fetch('http://127.0.0.1:5000/upload_image', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    // Handle the result
+                    setUploadedImageUrl(data['image_url']);
+                })
+                .catch((error) => {
+                    // Handle the error
+                });
+        }
+    };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            setSelectedImage(event.target.files[0]);
+        }
+    };
+
+    const handleParseText = () => {
+        if (uploadedImageUrl) {
+            fetch('http://127.0.0.1:5000/ocr', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    image_url: uploadedImageUrl,
+                }),
+            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    // Handle the result
+                    setParsedText(data['text']);
+                })
+                .catch((error) => {
+                    // Handle the error
+                });
+        }
+    }; // Add a closing curly brace here
+
+    return (
+        <div className="ImageOCR">
+            <form onSubmit={handleFormSubmit}>
+                <label>
+                    Image:
+                    <input type="file" name="image" onChange={handleImageChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            
+            {uploadedImageUrl && <img src={uploadedImageUrl} alt="Uploaded" />}
+
+            {uploadedImageUrl && <button onClick={handleParseText}>Parse Text</button>}
+            {parsedText && <p>{parsedText}</p>}
+        </div>
+    );
+}
+
+export default ImageOCR;
