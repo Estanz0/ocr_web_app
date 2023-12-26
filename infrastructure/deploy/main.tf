071e4319eaba80cb5c490c53b410d000c9b6b39b
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2023-12-26 23:30:49 +1300

    fa kv access

--- infrastructure/deploy/main.tf
+++ infrastructure/deploy/main.tf
@@ -103,7 +103,7 @@ resource "azurerm_linux_function_app" "default" {
 
 resource "azurerm_key_vault_access_policy" "default" {
   key_vault_id = azurerm_key_vault.default.id
-  tenant_id    = azurerm_linux_function_app.default.identity[0].tenant_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_linux_function_app.default.identity[0].principal_id
 
   key_permissions = [ "Create", "Delete", "Get", "List", "Update" ]
